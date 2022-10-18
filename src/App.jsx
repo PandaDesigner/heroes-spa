3b7c47442ad8b0f76e5d400cd3b365f5f9cb0b78
Pedro Fernandez <pand4designers@gmail.com>
2022-10-18 16:36:10 -0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
-import { Container, Box, CssBaseline, Typography } from '@mui/material'
+import { Container, Box, CssBaseline, Typography } from '@mui/material';
+import NavBar from './components/NavBar';
+
+
 
 const  App = () => {
 
 
   return (
-      <React.Fragment>
+    <React.Fragment>
+        <NavBar/>
       <CssBaseline />
       <Container maxWidth='100%'>
-
+        <Box sx={{}}>
+        <Typography variant='h2'>
+        Hola Mundo
+      </Typography>
+          </Box>
       </Container>
     </React.Fragment>
   );
---
+++ src/layout/LayoutPrincipal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import { Typography } from '@mui/material'
+
+export const LayoutPrincipal = () => {
+  return (
+        <>
+            <NavBar/>
+            <Typography>
+                desde el Layout Hola mundo
+            </Typography>
+        </>
+  )
+}
+
