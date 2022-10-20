ece105605dd409cc54c6ef220046d18b71a53975
Pedro Fernandez <pand4designers@gmail.com>
2022-10-19 21:16:55 -0500

    main

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,26 @@
+/**
+    * @description      : 
+    * @author           : 
+    * @group            : 
+    * @created          : 18/10/2022 - 21:51:15
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 18/10/2022
+    * - Author          : 
+    * - Modification    : 
+**/
+import { Container, Typography } from '@mui/material';
+
+
+export const HeroesApp = () => {
+  return (
+    <>
+    <Container>
+       <Typography variant='h2'xl={{}}>
+        Heroes App
+      </Typography>
+    </Container>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+/**
+    * @description      : 
+    * @author           : 
+    * @group            : 
+    * @created          : 18/10/2022 - 21:47:30
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 18/10/2022
+    * - Author          : 
+    * - Modification    : 
+**/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HeroesApp } from './HeroesApp';
+import styles from './styles.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HeroesApp />
   </React.StrictMode>
 )
