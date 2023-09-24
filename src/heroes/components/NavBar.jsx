5f4a1769a4e94f58312b853346933148e5e4f424
Pedro Fernandez <pedro@pandadesigners.com>
2023-09-24 12:44:44 -0500

    navbar

--- src/heroes/components/NavBar.jsx
+++ src/heroes/components/NavBar.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, Button, Box, IconButton } from '@mui/material'
-
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  IconButton,
+} from '@mui/material'
 
 const NavBar = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box sx={{flexGrow: 1}}>
+      <AppBar position='static'>
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{mr: 2}}
+          ></IconButton>
+          <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/heroes/pages/MarvelPages.jsx
+++ src/heroes/pages/MarvelPages.jsx
@@ -1,9 +1,11 @@
 // import {Typography} from '@mui/material'
 import {Container, Typography} from '@mui/material'
+import NavBar from '../components/NavBar'
 
 export const MarvelPages = () => {
   return (
     <>
+      <NavBar />
       <Container>
         <Typography variant='h1' component={'h1'}>
           Marvel
